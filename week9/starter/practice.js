28874b235a7c1e6e8f997f30316b788bf943f8fc
platard <platard@algonquincollege.com>
2024-07-03 12:53:43 -0400

    Week 9 initial and final code

---
+++ week9/starter/practice.js
@@ -0,0 +1,3 @@
+// 1. Refactor the cart section to dynamically add products to the cart using the "Buy" button and an event listener.
+
+// 2.Refactor the modal section to dynamically display product details and allow for updates to be saved using an event listener and a form element.
